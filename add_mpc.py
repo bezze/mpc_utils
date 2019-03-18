a8abe9d9d15c2332786ea5cb9267059e86e9eca6
Francisco Bezzecchi <franbcki@gmail.com>
2019-03-17 22:31:25 -0300

    Almost done. Need to make it portable. Env vars and stuff.

--- add_mpc.py
+++ add_mpc.py
@@ -2,6 +2,7 @@
 
 from io import StringIO
 import sys
+import re
 import os
 import json
 import subprocess as sp
@@ -12,21 +13,27 @@ import youtube_dl
 def get_best_audio(metadata):
     """docstring for get_best_audio"""
 
-    # audio_only = {fid: f for fid, f in formats.items() if "mime=audio" in f["url"]}
-    audio_only = {fid: f for fid, f in formats.items() if "audio" in f["format_note"]}
-    best_audio_list = sorted(audio_only.values(), key=lambda v: v["abr"], reverse=True)
+    formats = {f["format_id"]: f for f in metadata["formats"]}
+    audio_only = {
+        fid: f for fid,
+        f in formats.items() if f["vcodec"] == "none"}
+    best_audio_list = sorted(
+        audio_only.values(),
+        key=lambda v: v["abr"],
+        reverse=True)
+    print(best_audio_list)
     best_audio = best_audio_list[0]
 
     return best_audio
 
 
 class Playlist():
-    def __init__(self, file_path, stream_url):
+    def __init__(self, file_path, stream_url, author="", title="", duration=0):
         self.file_path = file_path
         self.stream_url = stream_url
-        self.author = ""
-        self.title = ""
-        self.duration = 0
+        self.author = author
+        self.title = title
+        self.duration = float(duration)
 
     def author(self, name):
         self.author = name
@@ -38,10 +45,11 @@ class Playlist():
         self.duration = length
 
     def write(self):
-        with open(self.file_path) as pl_file:
+        with open(self.file_path, "w") as pl_file:
             pl_file.write("#EXTM3U\n")
-            pl_file.write(f"#EXTINF:{self.duration},{self.author} - {self.title}\n")
-            pl_file.write(f"{self.stream_url}")
+            pl_file.write(
+                f"#EXTINF:{self.duration:.0f},{self.title} {self.author}\n")
+            pl_file.write(f"{self.stream_url}\n")
 
 
 class Capturing(list):
@@ -56,27 +64,66 @@ class Capturing(list):
         sys.stdout = self._stdout
 
 
-try:
-    with Capturing() as output:
-        youtube_dl._real_main(
-            ["-j", sys.argv[1]]
-        )
-except SystemExit:
-    pass
+def get_json():
+    """docstring for get_json"""
 
-metadata = json.loads(output[0])
+    try:
+        with Capturing() as output:
+            youtube_dl._real_main(
+                ["-j", sys.argv[1]]
+            )
+    except SystemExit:
+        pass
+    metadata = json.loads(output[0])
 
-title = metadata["title"]
-formats = {f["format_id"]: f for f in metadata["formats"]}
-best_audio = get_best_audio(metadata)
+    # with open("example.json") as f:
+    #     metadata = json.load(f)
 
-url = best_audio["url"]
+    return metadata
+
 # song_format =
 # sp.run("mpc -f {song_format} add {url}")
 
-cmd = ["/usr/bin/mpc"]
 # host = "192.168.1.4"
 # port = "6603""-h", f"{host}", "-p", f"{port}",
-cmd += ["add", f"{url}"]
 
-sp.run(cmd)
+
+def get_duration(audio):
+    """docstring for get_duration"""
+
+    url = audio["url"]
+    dur_regex = re.compile("dur=(\d*\.\d*)")
+    match = dur_regex.search(url)
+    if match:
+        duration = match.group(1)
+    else:
+        duration = 0
+
+    return duration
+
+
+def main():
+
+    metadata = get_json()
+    best_audio = get_best_audio(metadata)
+    url = best_audio["url"]
+    duration = get_duration(best_audio)
+
+    pl = Playlist("/home/user/.local/lib/mpd/playlists/stream_pl.m3u", url,
+                  title=metadata["title"],
+                  duration=duration
+                  )
+
+    pl.write()
+
+    cmd = ["/usr/bin/mpc"]
+    cmd += ["load", "stream_pl"]
+    sp.run(cmd)
+
+    cmd = ["/usr/bin/mpc"]
+    cmd += ["load", "stream_pl"]
+    sp.run(cmd)
+
+
+if __name__ == '__main__':
+    main()
